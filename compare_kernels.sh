2eb22c4e25835a17d8a62bcf0defab21faf9f3e4
char00les <char00les@gmail.com>
2020-09-23 11:55:59 -0400

    Create compare_kernels.sh

---
+++ compare_kernels.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Comment: Uses Perl
+
+LAST_KERNEL=$(rpm -q --last kernel | perl -pe 's/^kernel-(\S+).*/$1/' | head -1)
+CURRENT_KERNEL=$(uname -r)
+
+test $LAST_KERNEL = $CURRENT_KERNEL || echo REBOOT
